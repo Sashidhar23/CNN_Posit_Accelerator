98fe483dd102a0719c8811bec81f6499ac6aec0b
oishik1306 <oishik.ganguli@gmail.com>
2026-07-06 16:08:01 +0530

    Change ROWS and COLS parameters to 3

--- verilog/systolic/systolic_array.v
+++ verilog/systolic/systolic_array.v
@@ -3,8 +3,8 @@
 module systolic_array #(
     parameter N = 8,
     parameter ES = 1,
-    parameter ROWS = 6,
-    parameter COLS = 6
+    parameter ROWS = 3,
+    parameter COLS = 3
 )(
     input  wire                         clk,
     input  wire                         reset,
